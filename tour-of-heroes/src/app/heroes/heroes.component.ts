7e3ba1b9a8cadfac53278e2d4703f91a8c02ca42
ATerbrack <andrew.terbrack@gmail.com>
2024-11-12 15:19:01 -0500

    +pipes and imports

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { UpperCasePipe, CurrencyPipe } from '@angular/common'; // FIRST, Import this to ts file... 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [ UpperCasePipe, CurrencyPipe ], // THEN pass it to html file.
   templateUrl: './heroes.component.html',
   /*
   Alternatively...
@@ -13,6 +14,8 @@ import { Hero } from '../hero';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
+
+  value = 1.23;
 
   hero: Hero = { 
     id: 1, 
