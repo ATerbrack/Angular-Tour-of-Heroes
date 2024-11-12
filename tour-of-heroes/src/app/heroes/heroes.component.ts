d1583bc59ca162467ff7290ad35b03001555ebcb
ATerbrack <andrew.terbrack@gmail.com>
2024-11-12 15:26:24 -0500

    *2-way data binding

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { UpperCasePipe, CurrencyPipe } from '@angular/common'; // FIRST, Import this to ts file... 
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [ UpperCasePipe, CurrencyPipe ], // THEN pass it to html file.
+  imports: [ UpperCasePipe, CurrencyPipe, FormsModule, ], // THEN pass it to html file.
   templateUrl: './heroes.component.html',
   /*
-  Alternatively...
+  Alternatively, inject the html right into the TS.
   template: '<p>Hello, World! You can write html right in the template instead of refering to a different file.</p>',
   */
   styleUrl: './heroes.component.css'
