5603dbdf16e1197b22341533cc2b36ae95389225
ATerbrack <andrew.terbrack@gmail.com>
2024-11-14 11:00:01 -0500

    *rearrange

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -22,21 +22,22 @@ import { MessageService } from '../message.service';
 })
 
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
+  
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add('HeroesCompoent: Selected hero id=${hero.id}');
   }
 
-  constructor(private heroService: HeroService, private messageService: MessageService){}
-
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes); // Subscribe to listen asynchronously to the service
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
   }
 }
