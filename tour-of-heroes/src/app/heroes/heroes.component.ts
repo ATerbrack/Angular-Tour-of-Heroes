80b5aba752ab9315c5ec872a47924f0a75babe4a
ATerbrack <andrew.terbrack@gmail.com>
2024-11-14 10:59:16 -0500

    send message from heroes component

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
-import {NgIf, NgFor, UpperCasePipe,} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {HeroService} from '../hero.service';
+import { Component, OnInit } from '@angular/core';
+import { NgIf, NgFor, UpperCasePipe, } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeroService } from '../hero.service';
 
-import {Hero} from '../hero';
-import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
+import { Hero} from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { MessageService } from '../message.service';
 
 @Component({
   standalone: true,
@@ -20,15 +21,16 @@ import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
   ],
 })
 
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add('HeroesCompoent: Selected hero id=${hero.id}');
   }
 
-  constructor(private heroService: HeroService){}
+  constructor(private heroService: HeroService, private messageService: MessageService){}
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes); // Subscribe to listen asynchronously to the service
