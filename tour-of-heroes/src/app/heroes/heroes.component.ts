5ae8b664e341d47ff4fee10731fd12dc267b7265
ATerbrack <andrew.terbrack@gmail.com>
2024-11-14 10:09:50 -0500

    +nice hover-able buttons (which do nothing when clicked)

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
-import { Hero } from '../hero';
-import { UpperCasePipe, CurrencyPipe } from '@angular/common'; // FIRST, Import this to ts file... 
-import { FormsModule } from '@angular/forms';
-import { HEROES } from '../mock-heroes';
-import { NgFor } from '@angular/common';
+import {Component} from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
   standalone: true,
-  imports: [ UpperCasePipe, CurrencyPipe, FormsModule, NgFor, ], // THEN pass it to html file.
+  selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  /*
-  Alternatively, inject the html right into the TS.
-  template: '<p>Hello, World! You can write html right in the template instead of refering to a different file.</p>',
-  */
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css'],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+  ],
 })
+
 export class HeroesComponent {
-
   heroes = HEROES;
+  selectedHero?: Hero;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
