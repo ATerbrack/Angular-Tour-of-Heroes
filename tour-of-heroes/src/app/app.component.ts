fd05afd099916e737a775458657a5995927ccf1f
ATerbrack <andrew.terbrack@gmail.com>
2024-11-14 10:48:02 -0500

    + add messages.comp to app.comp

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root', // Name used in index.html to refer to this (app.component)
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent],
+  imports: [RouterOutlet, HeroesComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
