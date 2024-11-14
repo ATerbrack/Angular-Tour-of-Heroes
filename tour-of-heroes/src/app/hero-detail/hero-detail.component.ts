194abf345d5f42859cee70980754a65e5a108202
ATerbrack <andrew.terbrack@gmail.com>
2024-11-14 10:33:05 -0500

    *move details to subcomponent

--- tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, UpperCasePipe, NgIf, ],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
